6510aab42034cb7ba6def21c7257ce630c707aa8
bhesham <147453010+bheshamt@users.noreply.github.com>
2024-06-10 09:43:09 -0400

    Update Menu & Riddle.py

--- Menu & Riddle.py
+++ Menu & Riddle.py
@@ -9,7 +9,7 @@ def MainMenu():
         case "1":
             Riddles()
         case "2":
-            print("works")
+            Trivia()
         case _:
             print("Enter a 1 or 2\n")
             MainMenu()
@@ -82,10 +82,71 @@ def AgainRiddle(againridd):
         case _:
             print("Enter y or n.")
             AgainRiddle(againridd)
-            
+
+
+def Trivia():
+    global useranswer
+    global trivias
+    trivias = ["What was the last name of the first President of the United States?", "What year did the Titanic sink?", "What is the smallest bone in human body?"]
+    print("welcome to trivia questions!\n--------------------------------- \nYou have three chances to get the answer right.")
+    useranswer = str(input("\nChoose the difficulty level\n-------------\n1-Easy\n2-Medium\n3-Hard\nAnswer:"))
+    match useranswer:
+        case "1":
+            easytriv()
+        case "2":
+            medtriv()
+        case "3":
+            hardtriv()
+        case _:
+            print("try again and enter 1, 2 or 3")
+            Trivia()
+
+def easytriv():
+    askriddle = input(trivias[0])
+    if askriddle == "Washington" or askriddle == "washington":
+        print("Correct!")
+        againopt()
+    else:
+        print("Incorrect!")
+        againopt()
         
 
+def medtriv():
+    askriddle = input(trivias[1])
+    if askriddle == "1912":
+        print("Correct!")
+        againopt()
+    else:
+        print("Incorrect!")
+        againopt()
 
+def hardtriv():
+    askriddle = input(trivias[2])
+    if askriddle == "Stapes" or askriddle == "stapes":
+        print("Correct!")
+        againopt()
+    else:
+        print("Incorrect!")
+        againopt()
+    
+
+
+def againopt():
+    option= str(input("would you like to try another question? Y for yes and N for main menu: \nY/N: "))
+    match option:
+        case "Y":
+            return Trivia()
+        case "y":
+            return Trivia()
+        case "N":
+            return MainMenu()
+        case "n":
+            return MainMenu()
+        case _:
+            print("Enter y or n.")
+            againopt()
+            
+        
 
 
 
